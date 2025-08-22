4cdb3c850e07973eb60262b85ea3c9bfe59ac28f
mokurin000 <1348292515a@gmail.com>
2025-08-23 00:35:37 +0800

    feat: simplified chart

--- rating.py
+++ rating.py
@@ -2,21 +2,27 @@ import polars as pl
 
 pl.Config.set_tbl_rows(-1)
 
-pl.read_csv(
-    "rating_table.csv",
-    schema={
-        "target_rating": pl.Int32,
-        "rank": pl.String,
-        "achievement": pl.Int32,
-        "difficulty": pl.String,
-    },
-).with_columns(
-    pl.col("difficulty").cast(pl.Float64),
-).filter(
-    pl.lit("ABS").str.contains(
-        pl.col("rank").str.head(1),
+df = (
+    pl.scan_csv(
+        "rating_table.csv",
+        schema={
+            "target_rating": pl.Int32,
+            "rank": pl.String,
+            "achievement": pl.Int32,
+            "difficulty": pl.String,
+        },
     )
-).plot.scatter(
+    .with_columns(
+        pl.col("difficulty").cast(pl.Float64),
+    )
+    .filter(
+        pl.lit("ABS").str.contains(
+            pl.col("rank").str.head(1),
+        )
+    )
+)
+
+df.collect().plot.scatter(
     x="difficulty",
     y="target_rating",
     color="rank",
@@ -25,3 +31,13 @@ pl.read_csv(
     width=1600,
     height=1000,
 ).save("rating.html")
+
+df.filter(pl.col("rank").str.contains(r"\*").not_()).collect().plot.scatter(
+    x="difficulty",
+    y="target_rating",
+    color="rank",
+    size="achievement",
+).properties(
+    width=1600,
+    height=1000,
+).save("rating-simplified.html")
